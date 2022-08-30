7185a357dd2a96bf809a0aecf602704e9e0ee5ec
ErikPinel <erik.zusin@gmail.com>
2022-08-30 16:24:39 +0300

    f

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Register from './Components/Register/Register';
 import Contact from './component/Contact';
+import { useState,useRef ,useEffect} from 'react';
 
 function App() {
+  
+  const [firstName, setfirstName] = useState("")
+  const [resonContact, setresonContact] = useState("")
+  const [hearFromUs, sethearFromUs] = useState("")
   return (
     <div className="App">
       <Register />
-    <Contact></Contact>
-    </div>
+      <Contact userName={"user"} setfirstName={setfirstName} setresonContact={setresonContact} sethearFromUs={sethearFromUs} ></Contact>
+    <div>{firstName+resonContact+hearFromUs}</div>    </div>
   );
 }
 
--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,8 +1,17 @@
 import {Formik} from "formik";
 import * as Yup from "yup"
 
-const Contact= ()=>
+const Contact= (props)=>
 {
+
+    const save = (firstName,resonContact,hearFromUs )  => 
+    {
+        // event.preventDefault();
+        props.setfirstName(firstName)
+        props.setresonContact(resonContact)
+        props.sethearFromUs(hearFromUs)
+        
+    }
     const scheme = Yup.object().shape({
         firstName: Yup.string().required("firstName is a required"),
         resonContact: Yup.string().required("reason of contact is a required").max(100,"The reason of contact should have 100 charecters max")
@@ -12,14 +21,14 @@ const Contact= ()=>
     return(
 <div>
 
-<h1> contact</h1>
+<h1> hello  {props.userName} contact</h1>
     <Formik validationSchema={scheme} initialValues={{firstName:"",resonContact:"",hearFromUs: ""}} onSubmit={(values)=> alert(JSON.stringify(values))}>
         {({handleSubmit,handleChange,values,errors,handleBlur,touched})=>
         (
             <form  onSubmit={handleSubmit}  >
 <div>
-    <input type={"text"} name="firstName" placeholder="first Name" onChange={handleChange}
-    value={values.firstName} onBlur={handleBlur}/>
+    <input type={"text"} name="firstName" placeholder="first Name"  onChange={handleChange}
+    value={values.firstName} onBlur={handleBlur} />
      <div style={{color:'red'}}>{errors.firstName&& touched.firstName&& errors.firstName}</div>
 
 </div>
@@ -31,12 +40,12 @@ const Contact= ()=>
 </div>
 
 <div>
- <input type="text" name="hearFromUs"  maxlength="10" placeholder="How did you hear from us" onChange={handleChange}
-    value={values.hearFromUs} onBlur={handleBlur}/>
+ <input type="text" name="hearFromUs"  maxLength="100" placeholder="How did you hear from us" 
+    value={values.hearFromUs} onBlur={handleBlur}  onChange={handleChange}/>
 <div style={{color:'red'}}>{errors.hearFromUs&& touched.hearFromUs&& errors.hearFromUs}</div>
     
 </div>
-    <button type="submit" > submit</button>
+    <button type="submit" onClick={()=>save(values.firstName ,values.resonContact, values.hearFromUs )} > submit</button>
 
 
    
@@ -55,3 +64,6 @@ const Contact= ()=>
 )
 }
 export default Contact;
+
+// ,()=> e=>props.setresonContact(e.target.value)]
+//
